08fbbbd5fc1979703fd81468f68e2a9c3d1b42d4
sunset-hue <sunsetree@proton.me>
2025-11-11 21:01:31 -0800

    reworking stuff rn

---
+++ src/_inbuilt_types/pkgs.py
@@ -0,0 +1,4 @@
+class Package(str):
+    def __init__(self):
+        super().__init__()
+        self.pkg_name = self
--- src/installer.py
+++ src/installer.py
@@ -1,33 +1,43 @@
-"""Basic package installer, no versioning constraints, no solver yet"""
+"""Basic package installer"""
 
 import requests as r
-import json
-import zipfile as z
-import shutil
+from _inbuilt_types.pkgs import Package
 import os
+import json
+import sys
+import typing
 
 
-def download(pkg: str):
-    links = json.loads(
-        r.get(
-            f"https://pypi.org/simple/{pkg}/",
+# reworking everything to look cleaner
+
+
+def _request_return_url(pkg: Package):
+    return json.loads(
+        r.request(
+            "GET",
+            f"https://pypi.org/simple/{pkg.pkg_name}",
             headers={"Accept": "application/vnd.pypi.simple.v1+json"},
         ).content
-    )
-    downloaded = r.get(links["files"][0]["url"])
-    with open(f"{pkg}.whl", "wb") as f:
-        f.write(downloaded.content)
-    zipf = z.ZipFile(f"{pkg}.whl")
-    zipf.extractall(pkg)
-    try:
-        for i in os.listdir(f"{pkg}/{pkg}"):
-            shutil.move(f"{pkg}/{pkg}", f"site-packages/{pkg}")
-    except:
-        pass
-    os.removedirs(f"{pkg}")
-    os.remove(f"{pkg}.whl")
-
-    # only get first one to download for now
+    )["files"]
 
 
-print(download("rich"))
+def get_latest_ver(pkg: Package):
+    downloadables: dict = _request_return_url(pkg)
+    three_tuple: tuple[int, int, int] = (0, 0, 0)
+    curr_py_version = sys.version[
+        0:2
+    ]  # for example it would show us up to 3.13 if the current version was 3.13.5
+    # for caching versions so we can tell which one's the newest
+    for i in downloadables:
+        if i["requires_python"] == None or int(
+            "".join(curr_py_version.split("."))
+        ) >= int("".join(i["requires_python"].strip("<=>").split("."))):
+            # here, my thought process is to just strip all the dots and turn the first 2 numbers into a integer, so we can compare those 2 values (this might cause a problem with packages that require more precision with versioning though, but we'll handle that later)
+            to_be_turned = i["filename"].strip("abcdefghijklmnopqrstuvwxyz").split(".")
+            # first few results here (if package name isn't alpha numeric is the version numbers we need)
+            # since all pypi packages follow semver or some version of that, we can just put it into the 3 tuple, no extra formatting needed
+            three_tuple = (to_be_turned[0], to_be_turned[1], to_be_turned[2])
+    if three_tuple == (0, 0, 0):
+        print(
+            "Error! Could not find downloadable distribution that matches your current python version. Either you can downgrade or specify a version using ==, >=, <= syntax."
+        )
